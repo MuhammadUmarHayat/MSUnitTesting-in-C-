b6a3ebdfbce1321cf8cb1b9acdb10699a7fe8619
Hafiz Muhammad Umar Hayat <52619520+MuhammadUmarHayat@users.noreply.github.com>
2025-01-25 16:04:08 +0500

    Add files via upload

---
+++ BankAccountTests/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSUnitTestDemo
+{
+    public class BankAccount
+    {
+        //fields
+        string customer_name;
+        double balance;
+        public BankAccount(string name,double bal)
+        {
+            customer_name=name;
+            balance=bal;
+        }
+        //property
+        public double Balance 
+        { 
+            get { return balance; }
+        }
+        public void Debit(double amount)
+        {
+            if (balance.Equals(0))
+            {
+                throw new Exception("balance = 0");
+            }
+            if (amount <= 0 || amount > balance)
+            {
+                throw new ArgumentOutOfRangeException("amount <= 0 or amount > balance");
+            }
+            balance -= amount;
+
+
+        }
+        public void Credit(double amount)
+        {
+            if (amount <= 0 )
+            {
+                throw new ArgumentOutOfRangeException("amount<=0");
+            }
+
+            balance += amount;
+        }
+
+
+
+    }
+}
---
+++ BankAccountTests/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MSUnitTestDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bank_account = new BankAccount("Umar Hayat", 0);
+          
+            try
+            {
+                bank_account.Debit(5); // This should throw an ArgumentOutOfRangeException
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine("amount <= 0 or amount > balance");
+            }
+            catch (Exception e)
+            {
+                // If another exception is thrown, verify its message
+                Console.WriteLine("balance==0");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
